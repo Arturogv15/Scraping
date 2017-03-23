b17d24981c4d797fcb258656e4d0add782c0265a
Arturo Guzman <arturogv15@gmail.com>
2017-03-22 23:01:25 -0600

    Eliminar códigos anteriores

--- Scrapping.py
+++ Scrapping.py
@@ -7,7 +7,6 @@ cucei = "http://cucei.udg.mx/directorio"
 page = urllib.request.urlopen(cucei)
 soup = BeautifulSoup(page)
 
-
 items = soup.find_all('div', class_="item-list") 
 print(items[0].h3.string)
 print(items[0].ul.li.a.string)
@@ -37,44 +36,3 @@ for i in listaImagenes:
 		imagenes.append(i.img.src.string)
 
 print(imagenes[1])
-
-
-
-print("")
-print("")
-
-
-items_puesto=soup.find("div", class_="puesto-directorio").findAll()
-for i in items_puesto:
-	puesto = i.next_sibling
-print(puesto)
-
-for i in soup.find("div", class_="direccion-directorio").findAll():
-	direccion = i.nextSibling
-print(direccion)
-
-for i in soup.find("div", class_="views-field-field-conmutador").findAll():
-	conmutador = i.nextSibling
-print(conmutador)
-
-
-#itemsPuesto = soup.find_all('div', class_="puesto-directorio")
-#print(itemsPuesto[1].nextSibling)  
-#itemsDireccion = soup.find_all('div', class_="direccion-directorio")
-#print(itemsDireccion[0].div.text)
-#itemsTelefono = soup.find_all('div', class_="views-field-field-conmutador")
-#print(itemsTelefono[0].div.text)
-
-
-
-
-
-
-
-
-
-
-#for x in items:
-#	print(x.h3.string, " - ", x.ul.li.a.string)
-
-
