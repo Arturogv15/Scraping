6aa53e0d71a67ee185881bb90a5f6b21f0b524b9
Arturo Guzman <arturogv15@gmail.com>
2017-03-23 17:14:23 -0600

    Agregar las listas de fax, teléfonos e imágenes

--- Scrapping.py
+++ Scrapping.py
@@ -1,8 +1,8 @@
 import urllib.request
 from bs4 import BeautifulSoup
 import pandas
-cucei = "http://cucei.udg.mx/directorio"
-#cucei="file:///C:/Users/Arturo/Desktop/Directorio.html"
+#cucei = "http://cucei.udg.mx/directorio"
+cucei="file:///C:/Users/Arturo/Desktop/Directorio.html"
 
 page = urllib.request.urlopen(cucei)
 soup = BeautifulSoup(page)
@@ -20,40 +20,77 @@ print(puestogeneral[0].string)
 puesto =[] 
 listaPuestos = soup.find_all(class_="puesto-directorio")
 for i in listaPuestos:
-    if i.strong != None:
+    if i.strong == None or i.strong == " ":
+    	puesto.append("Sin valor")
+    else:
         puesto.append(i.strong.nextSibling)
 
 direccion = []
 listaDireccion = soup.find_all(class_="direccion-directorio")
 for i in listaDireccion:
-	if i.strong != None:
+	if i.strong == None or i.strong == " ":
+		direccion.append("Sin valor")
+	else:
 		direccion.append(i.strong.nextSibling)
+
+telefono = []
+listaTelefonos = soup.find_all(class_="telefono-directorio")
+for i in listaTelefonos:
+	if i.strong == None or i.strong == " ":
+		telefono.append("Sin valor")
+	else:
+		telefono.append(i.strong.nextSibling)
 
 conmutador = []
 listaConmutador = soup.find_all(class_="views-field-field-conmutador")
 for i in listaConmutador:
-	if i.strong != None or i.strong != " ":
+	if i.strong == None or i.strong == " ":
+		conmutador.append("Sin valor")
+	else:
 		conmutador.append(i.strong.nextSibling)
 
-#imagenes = []
-#listaImagenes = soup.find_all(class_="foto-directorio")
-#for i in listaImagenes:
-#	if i.img.src != None:
-#		imagenes.append(i.img.src.string)
-#
-#print(imagenes[1])
-print(len(puesto))
+
+fax = []
+listaFax = soup.find_all(class_="views-field-field-fax")
+for i in listaFax:
+	if i.strong == None or i.strong == " ":
+		fax.append("Sin valor")
+	else:
+		fax.append(i.strong.nextSibling)
+
+
+imagenes = []
+listaImagenes = soup.find_all(class_="foto-directorio")
+for i in listaImagenes:
+	if i.img['src'] != None:
+		imagenes.append(i.img['src'])
+
+fax = []
+listaFax = soup.find_all(class_="views-field-field-fax")
+for i in listaFax:
+	if i.strong == None or i.strong == " ":
+		fax.append("Sin valor")
+	else:
+		fax.append(i.strong.nextSibling)
+
+
+
 print(len(puestogeneral))
 print(len(nombre))
+print(len(puesto))
 print(len(direccion))
-print(len(conmutador))
+print(len(conmutador)) 
+print(len(imagenes))
 
 df = pandas.DataFrame(puestogeneral, columns=['Puesto General'])
 df['Titular']=nombre
 df['Puesto']=puesto
-df['Direccion']=direccion
-df['conmutador']=conmutador
-salida=open('cucei2.html', 'w')
-salida.write('<meta charset ="UTF-8">')
+df['Dirección']=direccion
+df['Teléfono']=telefono
+df['Conmutador']=conmutador
+df['Fax']=fax
+df['Imágenes']=imagenes
+salida=open('cucei.html', 'w')
+#salida.write('<meta charset ="UTF-8">')
 salida.write(df.to_html())
 salida.close()
